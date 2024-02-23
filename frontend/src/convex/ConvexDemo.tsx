f973da4545b2f3dffc6fc9001236a44868a28e5d
Lei Gao <gaolei@codelessuniverse.com>
2024-02-23 22:31:15 +0800

    feat: add mutation

--- frontend/src/convex/ConvexDemo.tsx
+++ frontend/src/convex/ConvexDemo.tsx
@@ -1,8 +1,22 @@
 "use client";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@convex/_generated/api";
+import { Button } from "@nextui-org/react";
 
 export default function ConvexDemo() {
   const tasks = useQuery(api.tasks.get);
-  return <main className="flex flex-col gap-4">{tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}</main>;
+  const createTask = useMutation(api.tasks.createTask);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-4">{tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}</div>
+      <Button
+        onPress={() => {
+          createTask({ text: Date.now() + "" });
+        }}
+      >
+        Add
+      </Button>
+    </div>
+  );
 }
