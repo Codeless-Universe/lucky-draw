0a2086737f1048701a4053b97890396855639f1b
Lei Gao <gaolei@codelessuniverse.com>
2024-03-01 09:52:00 +0800

    feat: add math.random

--- frontend/src/spinwheel/SpinWheelDom.tsx
+++ frontend/src/spinwheel/SpinWheelDom.tsx
@@ -84,7 +84,8 @@ export default function SpinWheelDom({ centerText = "Tap", ...props }: TProps_Sp
             }
 
             let temp = 3;
-            wheel.spinToItem(0, temp * 1000, true, 8);
+            let toIndex = Math.floor(Math.random() * props.labels.length);
+            wheel.spinToItem(toIndex, temp * 1000, true, 8);
             let func = () => {
               temp = temp - 0.1;
               if (temp < 0) {
